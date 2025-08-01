ea71dfeb1df90cf1a6730d03b2cb9bc01a9ffdc3
M. Gilang Chandrawinata <152847757+MGilangChandra@users.noreply.github.com>
2025-08-01 21:26:52 +0700

    update

--- src/components/sections/HeroSection.vue
+++ src/components/sections/HeroSection.vue
@@ -1,6 +1,6 @@
 <template>
     <section
-        class="text-[#FFFFF0] bg-[#13495A] h-[calc(100vh-68px)] md:h-screen flex md:justify-between px-[5%] md:px-[10%] xl:px-[20%] py-4 items-center flex-wrap-reverse">
+        class="text-[#FFFFF0] bg-[#13495A] h-screen flex md:justify-between px-[5%] md:px-[10%] xl:px-[20%] py-4 items-center flex-wrap-reverse">
         <div class="md:max-w-1/2 z-10">
             <h1 class="text-6xl font-extrabold text-shadow-[#000]">Your Complaint, Our Expertise</h1>
             <p>Entertainment, Tech, Graphic Design, Devices Troubleshooting, and Website Developing</p>
@@ -8,8 +8,8 @@
         <div class="md:max-w-1/2 absolute md:relative left-0 z-0">
             <img src="/assets/komputer-ilustrasi.webp" class="opacity-50 md:opacity-100 w-full" alt="">
         </div>
-        <a href="#blog"><i
-                class='bx bx-chevron-down absolute bottom-2 left-1/2 text-6xl -translate-x-1/2 z-10 duration-150 sm:text-[#2B2B35]'></i></a>
+        <a href="#blog">
+            <box-icon name='chevron-down' class="absolute bottom-2 left-1/2 text-6xl -translate-x-1/2 z-10 duration-150 fill-[#fffff0] sm:fill-[#2B2B35] size-[60px]"></box-icon></a>
         <div class="absolute -bottom-1 left-0 w-full overflow-hidden leading-[0] rotate-180">
             <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120"
                 preserveAspectRatio="none" class="relative block w-[calc(100%+1.3px)] h-[124]">
